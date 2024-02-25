8609dc3b9330fe2b6e1fae41138810f5234b6342
yurocarostyk <152785136+yurocarostyk@users.noreply.github.com>
2024-02-25 07:45:24 +0100

    corect

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
 function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
-  return 'You ordered ${quantity} droids worth ${totalPrice} credits!';
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
